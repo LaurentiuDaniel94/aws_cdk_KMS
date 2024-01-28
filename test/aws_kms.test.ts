98ce8a6d3853aba4ca612c1d89341f25fc04ff33
Laurentiu Daniel Ginghina <laurentiuginghina@outlook.com>
2024-01-28 11:25:35 +0200

    Initial commit

---
+++ test/aws_kms.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsKms from '../lib/aws_kms-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws_kms-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsKms.AwsKmsStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
