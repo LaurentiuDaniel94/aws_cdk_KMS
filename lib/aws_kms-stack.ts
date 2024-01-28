840c156d5a85b6a2292655d24d0b673856ff18c8
Laurentiu Daniel Ginghina <laurentiuginghina@outlook.com>
2024-01-28 11:44:42 +0200

    wip

--- lib/aws_kms-stack.ts
+++ lib/aws_kms-stack.ts
@@ -1,16 +1,28 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as kms from "aws-cdk-lib/aws-kms";
+import * as s3 from "aws-cdk-lib/aws-s3";
 
 export class AwsKmsStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create KMS Key
+    const testKmsKey = new kms.Key(this, "testKmsKey", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      pendingWindow: cdk.Duration.days(7),
+      alias: "alias/testKey",
+      description: "Test KMS Key",
+      enableKeyRotation: false,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'AwsKmsQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    //Create s3 Bucket
+    const s3BucketKMS = new s3.Bucket(this, "testS3BucketKMS", {
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+      encryption: s3.BucketEncryption.KMS,
+      encryptionKey: testKmsKey,
+      bucketName: "test-s3-bucket-kms",
+    });
+
   }
 }
